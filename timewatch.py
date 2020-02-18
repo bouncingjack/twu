512bf36aff3fc4a5e81814edefc9ec64ccfa49be
Jack German <yakovger@gmail.com>
2020-02-18 11:39:29 +0200

    add main timewatch module

---
+++ timewatch.py
@@ -0,0 +1,29 @@
+# imports
+
+class Report:
+    """
+    Represnets the time watch report.
+    This will call updating actions.
+    """
+    
+    def __init__(self, start_date, end_date, chrome_driver, force_times=None):
+        pass
+        
+    def __call__(self):
+        pass
+
+
+class ChromeWebDriver:
+    """
+    Subclass for the chromedriver - path container
+    """
+
+    def __init__(self, executable_path):
+        pass
+
+
+
+
+if __name__ == '__main__':
+    # call report actions here
+    pass
